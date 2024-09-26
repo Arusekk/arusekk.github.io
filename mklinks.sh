8303aedae094ce3a718df7fc9002c546842a24d7
Arusekk <arek_koz@o2.pl>
2024-09-26 11:15:49 +0200

    MDN says this is safe

--- mklinks.sh
+++ mklinks.sh
@@ -2,7 +2,7 @@
 
 cd public
 
-export HTTEMPLATE='<!DOCTYPE html><html><meta http-equiv=refresh content="1;%s"><script>location="%s"+location.hash</script><a href="%s">Moved'
+export HTTEMPLATE='<!DOCTYPE html><html><meta http-equiv=refresh content="0;url=%s"><script>location.replace("%s"+location.hash)</script><a href="%s">Moved'
 find * -type d -exec sh -c '[ ! -f $0/index.html ] || printf "$HTTEMPLATE" "${0##*/}/" "${0##*/}/" "${0##*/}/" >$0.html' '{}' \;
 find -name index.html -print0 | xargs -0 sed -i 's@href="\([^/".]*\)\.md"@href="../\1/"@g'
 ln -fs posts/index.xml atom.xml
