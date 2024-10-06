5c0a85637e5d260cdf0c4ce93c8f71454a8730ec
Arusekk <floss@arusekk.pl>
2024-10-07 00:47:17 +0200

    robots: enable robots.txt

--- mklinks.sh
+++ mklinks.sh
@@ -5,4 +5,4 @@ cd public
 export HTTEMPLATE='<!DOCTYPE html><html><meta http-equiv=refresh content="0;url=%s"><script>location.replace("%s"+location.hash)</script><a href="%s">Moved'
 find * -type d -exec sh -c '[ ! -f $0/index.html ] || printf "$HTTEMPLATE" "${0##*/}/" "${0##*/}/" "${0##*/}/" >$0.html' '{}' \;
 find -name index.html -print0 | xargs -0 sed -i 's@href="\([^/".]*\)\.md"@href="../\1/"@g'
-ln -fs posts/index.xml atom.xml
+ln -f posts/index.xml atom.xml
